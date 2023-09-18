b3b736a473df5a8f5997e7f10ee7acf80e6fe17b
Sathsara Hewage <sathsarahewage@gmail.com>
2023-09-18 17:10:03 +0530

    Update login.dart

--- happening_app/login_pages/lib/screens/login.dart
+++ happening_app/login_pages/lib/screens/login.dart
@@ -21,25 +21,132 @@ class Login extends StatelessWidget {
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                const Text(
-                  'Hi!',
-                  style: TextStyle(color: Colors.white, fontSize: 70),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30.0),
+                const SizedBox(
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Hi!',
+                      style: TextStyle(
+                          fontSize: 70,
+                          color: Color.fromARGB(234, 255, 255, 255),
+                          fontWeight: FontWeight.bold),
                     ),
-                    filled: true,
-                    hintStyle: TextStyle(color: Colors.grey[800]),
-                    hintText: "Email",
-                    fillColor: Colors.white,
                   ),
                 ),
-                TextButton.icon(
-                  onPressed: () {},
-                  icon: Image.asset('assets/images/facebook.png', width: 15,),
-                  label: const Text('Continue with facebook'),
+                const SizedBox(
+                  height: 5,
+                ),
+                //Email-Box//
+                SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      filled: true,
+                      hintStyle: TextStyle(color: Colors.grey[800]),
+                      hintText: "Email",
+                      fillColor: Colors.white,
+                    ),
+                  ),
+                ),
+                //Continue Button//
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      backgroundColor: Colors.blue,
+                    ),
+                    child: const Text(
+                      'Continue',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                //OR//
+                const SizedBox(
+                  child: Text(
+                    'or',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                //fb-log
+                SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: ElevatedButton.icon(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      backgroundColor: Colors.white,
+                    ),
+                    icon: Image.asset(
+                      'assets/images/facebook.png',
+                      width: 25,
+                    ),
+                    label: const Text(
+                      'Continue with Facebook',
+                      style: TextStyle(fontSize: 20, color: Colors.black),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                //google-log
+                SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: ElevatedButton.icon(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      backgroundColor: Colors.white,
+                    ),
+                    icon: Image.asset(
+                      'assets/images/google.png',
+                      width: 25,
+                    ),
+                    label: const Text(
+                      'Continue with Google',
+                      style: TextStyle(fontSize: 20, color: Colors.black),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                //apple-log
+                SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: ElevatedButton.icon(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      backgroundColor: Colors.white,
+                    ),
+                    icon: Image.asset(
+                      'assets/images/apple.png',
+                      width: 25,
+                    ),
+                    label: const Text(
+                      'Continue with Apple',
+                      style: TextStyle(fontSize: 20, color: Colors.black),
+                    ),
+                  ),
                 ),
               ],
             ),
