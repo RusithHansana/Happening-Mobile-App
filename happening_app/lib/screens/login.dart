b3af071a6f1b0da7fcbf79bfca495e4c9ff8e19b
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2023-09-21 16:03:08 +0530

    Update login.dart

--- happening_app/lib/screens/login.dart
+++ happening_app/lib/screens/login.dart
@@ -20,222 +20,226 @@ class Login extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 30),
           child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      'Hi!',
-                      style: TextStyle(
-                          fontSize: 65,
-                          color: Colors.white.withOpacity(0.9),
-                          fontWeight: FontWeight.bold),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Hi!',
+                        style: TextStyle(
+                            fontSize: 65,
+                            color: Colors.white.withOpacity(0.9),
+                            fontWeight: FontWeight.bold),
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 3,
-                ),
-                //Email-Box//
-                GlassBox(
-                  borderRadius: 32,
-                  child: Padding(
-                    padding: const EdgeInsets.all(30),
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          width: 350,
-                          height: 50,
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(30.0),
-                              ),
-                              contentPadding: const EdgeInsets.symmetric(
-                                vertical: 16,
-                                horizontal: 32,
-                              ),
-                              filled: true,
-                              hintStyle: TextStyle(color: Colors.grey[800]),
-                              hintText: "Email",
-                              fillColor: Colors.white,
-                            ),
-                          ),
-                        ),
-
-                        //Continue Button//
-                        const SizedBox(height: 10),
-                        SizedBox(
-                          width: 350,
-                          height: 50,
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(25),
-                              ),
-                              backgroundColor: Colors.blue,
-                            ),
-                            child: const Text(
-                              'Continue',
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.w400),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 12.67),
-                        //OR//
-                        const SizedBox(
-                          child: Text(
-                            'or',
-                            style: TextStyle(color: Colors.white, fontSize: 14),
-                          ),
-                        ),
-                        const SizedBox(height: 12.67),
-                        //fb-log
-                        SizedBox(
-                          width: 350,
-                          height: 50,
-                          child: ElevatedButton.icon(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(25),
-                              ),
-                              backgroundColor: Colors.white,
-                            ),
-                            icon: Image.asset(
-                              'assets/images/facebook.png',
-                              width: 24,
-                            ),
-                            label: const Text(
-                              'Continue with Facebook',
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w400),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        //google-log
-                        SizedBox(
-                          width: 350,
-                          height: 50,
-                          child: ElevatedButton.icon(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(25),
-                              ),
-                              backgroundColor: Colors.white,
-                            ),
-                            icon: Image.asset(
-                              'assets/images/google.png',
-                              width: 24,
-                            ),
-                            label: const Text(
-                              'Continue with Google',
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w400),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        //apple-log
-                        SizedBox(
-                          width: 350,
-                          height: 50,
-                          child: ElevatedButton.icon(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(25),
-                              ),
-                              backgroundColor: Colors.white,
-                            ),
-                            icon: Image.asset(
-                              'assets/images/apple.png',
-                              width: 24,
-                              height: 24,
-                            ),
-                            label: const Text(
-                              'Continue with Apple',
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w400),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 16),
-
-                        Row(
-                          mainAxisAlignment:
-                              MainAxisAlignment.start, // Left-align the content
-                          children: [
-                            Text.rich(
-                              TextSpan(
-                                text: 'Don’t you have an account? ',
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.white,
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  //Email-Box//
+                  GlassBox(
+                    borderRadius: 32,
+                    child: Padding(
+                      padding: const EdgeInsets.all(30),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            width: 350,
+                            height: 50,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30.0),
                                 ),
-                                children: <TextSpan>[
-                                  TextSpan(
-                                    text: 'Sign Up',
-                                    style: const TextStyle(
-                                      fontSize: 16,
-                                      color: Color(0xFFF8AB05),
-                                    ),
-                                    recognizer: TapGestureRecognizer()
-                                      ..onTap = () {
-                                        // Add your action when the clickable part is tapped.
-                                        // You can navigate to a new screen, show a dialog, etc.
-                                        // For example, let's print a message to the console.
-                                        print('Clickable text tapped!');
-                                      },
-                                  ),
-                                ],
+                                contentPadding: const EdgeInsets.symmetric(
+                                  vertical: 16,
+                                  horizontal: 32,
+                                ),
+                                filled: true,
+                                hintStyle: TextStyle(color: Colors.grey[800]),
+                                hintText: "Email",
+                                fillColor: Colors.white,
                               ),
                             ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 3,
-                        ),
-                        Row(
-                          mainAxisAlignment:
-                              MainAxisAlignment.start, // Left-align the content
-                          children: [
-                            GestureDetector(
-                              onTap: () {
-                                Navigator.pushNamed(context, '/forgotPassword');
-                              },
+                          ),
+
+                          //Continue Button//
+                          const SizedBox(height: 10),
+                          SizedBox(
+                            width: 350,
+                            height: 50,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                                backgroundColor: Colors.blue,
+                              ),
                               child: const Text(
-                                'Forgot Password?',
+                                'Continue',
                                 style: TextStyle(
-                                  fontSize: 16,
-                                  color: Color(0xFFF8AB05),
-                                ),
+                                    fontSize: 16, fontWeight: FontWeight.w400),
                               ),
                             ),
-                          ],
-                        ),
+                          ),
+                          const SizedBox(height: 12.67),
+                          //OR//
+                          const SizedBox(
+                            child: Text(
+                              'or',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 14),
+                            ),
+                          ),
+                          const SizedBox(height: 12.67),
+                          //fb-log
+                          SizedBox(
+                            width: 350,
+                            height: 50,
+                            child: ElevatedButton.icon(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                                backgroundColor: Colors.white,
+                              ),
+                              icon: Image.asset(
+                                'assets/images/facebook.png',
+                                width: 24,
+                              ),
+                              label: const Text(
+                                'Continue with Facebook',
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          //google-log
+                          SizedBox(
+                            width: 350,
+                            height: 50,
+                            child: ElevatedButton.icon(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                                backgroundColor: Colors.white,
+                              ),
+                              icon: Image.asset(
+                                'assets/images/google.png',
+                                width: 24,
+                              ),
+                              label: const Text(
+                                'Continue with Google',
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          //apple-log
+                          SizedBox(
+                            width: 350,
+                            height: 50,
+                            child: ElevatedButton.icon(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                                backgroundColor: Colors.white,
+                              ),
+                              icon: Image.asset(
+                                'assets/images/apple.png',
+                                width: 24,
+                                height: 24,
+                              ),
+                              label: const Text(
+                                'Continue with Apple',
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 16),
 
-                        //link section
-                      ],
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment
+                                .start, // Left-align the content
+                            children: [
+                              Text.rich(
+                                TextSpan(
+                                  text: 'Don’t you have an account? ',
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                  ),
+                                  children: <TextSpan>[
+                                    TextSpan(
+                                      text: 'Sign Up',
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        color: Color(0xFFF8AB05),
+                                      ),
+                                      recognizer: TapGestureRecognizer()
+                                        ..onTap = () {
+                                          // Add your action when the clickable part is tapped.
+                                          // You can navigate to a new screen, show a dialog, etc.
+                                          // For example, let's print a message to the console.
+                                          print('Clickable text tapped!');
+                                        },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 3,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment
+                                .start, // Left-align the content
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.pushNamed(
+                                      context, '/forgotPassword');
+                                },
+                                child: const Text(
+                                  'Forgot Password?',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFFF8AB05),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+
+                          //link section
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
