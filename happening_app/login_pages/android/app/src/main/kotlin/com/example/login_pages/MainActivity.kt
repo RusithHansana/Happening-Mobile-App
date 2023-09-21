5b46b046c517f7844a262fd51a8ccb4e855c1d0b
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2023-09-21 12:20:11 +0530

    login page updated

--- happening_app/login_pages/android/app/src/main/kotlin/com/example/login_pages/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.login_pages
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- happening_app/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ happening_app/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\saths\OneDrive\Documents\Year3\MAD\Git\Happening-Mobile-App\happening_app"
+export "FLUTTER_APPLICATION_PATH=C:\Users\avish\OneDrive\Documents\Happening Project\Happening\Happening-Mobile-App\happening_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.1.0"
-export "FLUTTER_BUILD_NUMBER=0.1.0"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
